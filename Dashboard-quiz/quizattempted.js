a0ab7d85e17aafe336e4f1a4f12247928f8c5728
Sharukesh-M <murugesansharukesh@gmail.com>
2025-02-26 01:00:47 +0530

    Add files via upload

---
+++ Dashboard-quiz/quizattempted.js
@@ -0,0 +1,10 @@
+const quizattempt=[
+    {
+        QuizTopic:'JavaScript',
+        QuizId: '0112',
+        Score:'7/10',
+        Status:'Pending',
+        Timing:'35/60min',
+        Details:'view'
+    }
+]
